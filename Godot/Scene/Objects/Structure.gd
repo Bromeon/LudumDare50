a1374e67c2cef84a0d6f0efe59fa69caccdfbb61
Jan Haller <bromeon@gmail.com>
2022-04-02 20:36:15 +0200

    Include roof in collision

--- Godot/Scene/Objects/Structure.gd
+++ Godot/Scene/Objects/Structure.gd
@@ -2,5 +2,5 @@ extends Spatial
 
 
 func applyMaterial(mat: SpatialMaterial) -> void:
-	$Roof.set_surface_material(0, mat)
-	$Core.set_surface_material(0, mat)
+	$Roof/Mesh.set_surface_material(0, mat)
+	$Core/Mesh.set_surface_material(0, mat)
