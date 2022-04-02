a5ac207d2d321863ef31bd30949894b102055cc5
setzer22 <jsanchezfsms@gmail.com>
2022-04-02 22:10:26 +0200

    Dilation using ndarray

--- Rust/src/godot/terrain.rs
+++ Rust/src/godot/terrain.rs
@@ -2,13 +2,14 @@ use gdnative::{
 	api::{GlobalConstants, ImageTexture, Mesh, MeshInstance, PlaneMesh, ShaderMaterial},
 	prelude::*,
 };
+use ndarray::{s, Array2};
 use noise::{NoiseFn, Perlin};
 
 #[derive(NativeClass, Debug)]
 #[inherit(Node)]
 pub struct Terrain {
 	pub mesh: Option<Ref<MeshInstance>>,
-	pub image_data: ByteArray,
+	array: TerrainArray,
 }
 
 macro_rules! get_node {
@@ -29,13 +30,14 @@ impl Terrain {
 	fn new(_base: &Node) -> Self {
 		Self {
 			mesh: None,
-			image_data: ByteArray::from_iter(std::iter::repeat(128u8).take(256 * 256)),
+			array: TerrainArray::default(),
 		}
 	}
 
 	fn reload_image(&mut self) {
 		let image = Image::new().into_shared();
-		image.create_from_data(256, 256, false, Image::FORMAT_L8, self.image_data.clone());
+		let bytes = ByteArray::from_slice(self.array.data().slice(s![.., ..]).as_slice().unwrap());
+		image.create_from_data(256, 256, false, Image::FORMAT_L8, bytes);
 
 		let material = self
 			.mesh
@@ -55,19 +57,97 @@ impl Terrain {
 	fn _ready(&mut self, base: &Node) {
 		let mesh = get_node!(base, "Mesh", MeshInstance);
 		self.mesh = Some(mesh);
+
+		self.array.fill_shape(
+			Shape::Circle {
+				center: [50, 50],
+				radius: 10,
+			},
+			BLIGHT,
+		);
+
 		self.reload_image()
 	}
 
 	#[export]
 	fn _process(&mut self, _base: &Node, _dt: f32) {
-		let noise = Perlin::new();
-		self.image_data = ByteArray::from_iter((0..256).flat_map(|i| {
-			(0..256).map(move |j| {
-				use std::f64::consts::SQRT_2;
-				let noise = noise.get([i as f64 / 16.0, j as f64 / 16.0]);
-				(256.0 * (noise + (SQRT_2 / 2.0)) / SQRT_2) as u8
-			})
-		}));
+		self.array.dilate();
 		self.reload_image();
+	}
+}
+
+#[derive(Debug)]
+pub struct TerrainArray {
+	data: ndarray::Array2<u8>,
+}
+
+pub const BLIGHT: u8 = u8::MAX;
+pub const CLEAN: u8 = 0u8;
+
+pub enum Shape {
+	Circle { center: [usize; 2], radius: usize },
+}
+
+impl TerrainArray {
+	pub const WIDTH: usize = 256;
+	pub const HEIGHT: usize = 256;
+
+	pub fn new() -> Self {
+		Self {
+			data: ndarray::Array2::from_elem((Self::WIDTH, Self::HEIGHT), CLEAN),
+		}
+	}
+
+	pub fn fill_shape(&mut self, shape: Shape, fill: u8) {
+		match shape {
+			Shape::Circle { center, radius } => {
+				let window_size = radius * 2 + 1;
+				let wi = center[0].saturating_sub(radius);
+				let wj = center[1].saturating_sub(radius);
+				self.data
+					.slice_mut(s![wi..wi + window_size, wj..wj + window_size])
+					.indexed_iter_mut()
+					.for_each(|((i, j), value)| {
+						let di = radius as isize - i as isize;
+						let dj = radius as isize - j as isize;
+						let dist_sq = (di * di + dj * dj) as usize;
+						if dist_sq <= radius * radius {
+							*value = fill
+						}
+					});
+			}
+		}
+	}
+
+	pub fn dilate(&mut self) {
+		let mut new_data = ndarray::Array2::zeros(self.data.raw_dim());
+		ndarray::Zip::from(new_data.slice_mut(s![2..254, 2..254]))
+			.and(self.data.windows((5, 5)))
+			.for_each(|v, window| {
+				let kernel = ndarray::array![
+					[0, 1, 1, 1, 0],
+					[1, 1, 1, 1, 1],
+					[1, 1, 1, 1, 1],
+					[1, 1, 1, 1, 1],
+					[0, 1, 1, 1, 0],
+				];
+				*v = ndarray::Zip::from(window)
+					.and(&kernel)
+					.fold(
+						CLEAN,
+						|acc, val, k| acc.max(*val * k),
+					);
+			});
+		self.data = new_data;
+	}
+
+	pub fn data(&self) -> &Array2<u8> {
+		&self.data
+	}
+}
+
+impl Default for TerrainArray {
+	fn default() -> Self {
+		Self::new()
 	}
 }
