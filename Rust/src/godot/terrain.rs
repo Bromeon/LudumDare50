6286399dcc327677326941d16194677b6500f4c8
setzer22 <jsanchezfsms@gmail.com>
2022-04-02 18:26:12 +0200

    Simple terrain texture splatting. Set up LFS

--- Rust/src/godot/terrain.rs
+++ Rust/src/godot/terrain.rs
@@ -2,14 +2,13 @@ use gdnative::{
 	api::{GlobalConstants, ImageTexture, Mesh, MeshInstance, PlaneMesh, ShaderMaterial},
 	prelude::*,
 };
+use noise::{NoiseFn, Perlin};
 
-#[derive(NativeClass, Debug, Default)]
+#[derive(NativeClass, Debug)]
 #[inherit(Node)]
 pub struct Terrain {
-	#[property]
-	pub unimplemented: i32,
-
 	pub mesh: Option<Ref<MeshInstance>>,
+	pub image_data: ByteArray,
 }
 
 macro_rules! get_node {
@@ -28,28 +27,47 @@ macro_rules! get_node {
 #[methods]
 impl Terrain {
 	fn new(_base: &Node) -> Self {
-		Self::default()
+		Self {
+			mesh: None,
+			image_data: ByteArray::from_iter(std::iter::repeat(128u8).take(256 * 256)),
+		}
+	}
+
+	fn reload_image(&mut self) {
+		let image = Image::new().into_shared();
+		image.create_from_data(256, 256, false, Image::FORMAT_L8, self.image_data.clone());
+
+		let material = self
+			.mesh
+			.unwrap()
+			.get_surface_material(0)
+			.unwrap()
+			.cast::<ShaderMaterial>()
+			.unwrap();
+		let texture = material
+			.get_shader_param("Splatmap")
+			.try_to_object::<ImageTexture>()
+			.unwrap();
+		texture.create_from_image(image, Texture::FLAGS_DEFAULT);
 	}
 
 	#[export]
 	fn _ready(&mut self, base: &Node) {
 		let mesh = get_node!(base, "Mesh", MeshInstance);
 		self.mesh = Some(mesh);
+		self.reload_image()
+	}
 
-		let data = ByteArray::from_iter(std::iter::repeat(128u8).take(256 * 256));
-		let image = Image::new().into_shared();
-		image.create_from_data(256, 256, false, Image::FORMAT_L8, data);
-
-		let material = mesh
-			.get_surface_material(0)
-			.unwrap()
-			.cast::<ShaderMaterial>()
-			.unwrap();
-		dbg!(&material.get_shader_param("Splatmap"));
-		let texture = material
-			.get_shader_param("Splatmap")
-			.try_to_object::<ImageTexture>()
-			.unwrap();
-		texture.create_from_image(image, Texture::FLAGS_DEFAULT);
+	#[export]
+	fn _process(&mut self, _base: &Node, _dt: f32) {
+		let noise = Perlin::new();
+		self.image_data = ByteArray::from_iter((0..256).flat_map(|i| {
+			(0..256).map(move |j| {
+				use std::f64::consts::SQRT_2;
+				let noise = noise.get([i as f64 / 16.0, j as f64 / 16.0]);
+				(256.0 * (noise + (SQRT_2 / 2.0)) / SQRT_2) as u8
+			})
+		}));
+		self.reload_image();
 	}
 }
