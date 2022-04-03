31cfa0ca645c91b563286f16eda6b59007c7a933
Jan Haller <bromeon@gmail.com>
2022-04-03 15:22:34 +0200

    Floating text

---
+++ Godot/Scene/Util/Text3D.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+export(String) var text setget _setString
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _setString(s: String) -> void:
+	print("Set string: ", s)
+	$CanvasLayer/Label.text = s
+
+
+
+func _process(_delta):
+	var cam = get_viewport().get_camera()
+	var pos2d = cam.unproject_position(get_global_transform().origin)
+
+	var label = $CanvasLayer/Label
+	label.rect_position = pos2d - label.rect_size / 2.0
+
+	#print("2D Pos: ", pos2d)
