0a65e153758f08492e821e20e6b40fc154abfb1b
Jan Haller <bromeon@gmail.com>
2022-04-04 11:04:45 +0200

    AddStructure .gdns + .rs

---
+++ Rust/src/godot/add_structure.rs
@@ -0,0 +1,20 @@
+use gdnative::prelude::*;
+
+#[derive(NativeClass, Debug, Default, Clone)]
+pub struct AddStructure {
+	#[property]
+	pub position: Vector3,
+
+	#[property]
+	pub structure_ty: String,
+
+	#[property]
+	pub pipe_from_obj: Option<Ref<Spatial>>
+}
+
+#[methods]
+impl AddStructure {
+	fn new(_base: &Reference) -> Self {
+		Self::default()
+	}
+}
